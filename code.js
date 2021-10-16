ba0a69f521394a8679124e0e1099a461bb7b5a27
Ayaj003 <92650464+Ayaj003@users.noreply.github.com>
2021-10-16 23:39:37 +0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,28 @@
+//array
+let data = {
+name :"Aya",
+age :18,
+address:"Nablus",
+email:"aya.ja@outlook.com",
+id:123455,
+}
+
+if(data['age']<18){
+    console.log("under age");
+}
+
+data['id']=1234567890;
+
+let num=[1,2,3]; 
+
+//function
+
+function print(){
+    console.log("Hello world");
+}
+print();
+
+function MyName(name){
+console.log("My name is"+" "+name);
+}
+MyName("Aya");
